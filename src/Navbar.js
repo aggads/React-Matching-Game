1589da12c827f4006ab3ce5533fe228e8147a384
Samy <43170730+aggads@users.noreply.github.com>
2019-01-07 15:51:58 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,11 @@ import React from 'react'
 import './Navbar.css';
 
 
-const Navbar = () => {
-		return (
+const Navbar = () => (
 			<nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 			<p className="navbar-brand">Matching Game</p>
 			<span className="navbar-toggler-icon"></span>
 			</nav>
 			)
-	}
-
 
 export default Navbar
